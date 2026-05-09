1403ca9a8a027cb3ed60eebcb44622147b800949
michelzaum <michel.oliveira999@hotmail.com>
2026-05-09 10:50:11 -0300

    feat: add field validations in AddEmployeeDto Model

--- EmployeeAdminPortal/Models/AddEmployeeDto.cs
+++ EmployeeAdminPortal/Models/AddEmployeeDto.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace EmployeeAdminPortal.Models;
 
 public class AddEmployeeDto
 {
+  [Required(ErrorMessage = "Name is required")]
+  [StringLength(100)]
   public required string Name { get; set; }
+  
+  [Required]
+  [EmailAddress(ErrorMessage = "Invalid e-mail format")]
   public required string Email { get; set; }
+  
+  [Phone]
   public string? Phone { get; set; }
+  
+  [Range(0, 1000000)]
   public decimal Salary { get; set; }
 }
