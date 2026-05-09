b24ae322bebb7c11eb991a20f54b44a5c9dfda8f
michelzaum <michel.oliveira999@hotmail.com>
2026-05-08 21:05:13 -0300

    feat: add Sql Server database connection using EntityFrameworCore

---
+++ EmployeeAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Data
+{
+  public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+  {
+    public DbSet<Employee> Employees { get; set; }
+  }
+}
