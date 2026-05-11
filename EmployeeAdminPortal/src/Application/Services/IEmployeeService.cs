4fd2e7527e5323ae0bce1bb503d3208bb5627ffd
michelzaum <michel.oliveira999@hotmail.com>
2026-05-10 23:02:52 -0300

    feat: create IEmployeeService interface in Application layer

---
+++ EmployeeAdminPortal/src/Application/Services/IEmployeeService.cs
@@ -0,0 +1,14 @@
+using EmployeeAdminPortal.Domain.Entities;
+using EmployeeAdminPortal.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace EmployeeAdminPortal.Application.Services;
+
+public interface IEmployeeService
+{
+  Task<List<Employee>> GetAllAsync();
+  Task<Employee?> GetByIdAsync(Guid id);
+  Task<EntityEntry<Employee>> CreateAsync(Employee employee);
+  Task<int> UpdateAsync(Guid id, UpdateEmployeeDto updateEmployeeDto);
+  Task DeleteAsync(Guid id);
+}
