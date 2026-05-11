fb06f07b7ba594e17eb7f13ac7b43038bb282282
michelzaum <michel.oliveira999@hotmail.com>
2026-05-10 23:12:35 -0300

    feat: implement EmployeeService class in Application layer

---
+++ EmployeeAdminPortal/src/Application/Services/EmployeeService.cs
@@ -0,0 +1,35 @@
+using EmployeeAdminPortal.Application.Services.Interfaces;
+using EmployeeAdminPortal.Domain.Entities;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Infra.Repositories;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace EmployeeAdminPortal.Application.Services;
+
+public class EmployeeService(EmployeeRepository employeeRepository) : IEmployeeService
+{
+  public async Task<List<Employee>> GetAllAsync()
+  {
+    return await employeeRepository.GetAllAsync();
+  }
+
+  public async Task<Employee?> GetByIdAsync(Guid id)
+  {
+    return await employeeRepository.GetByIdAsync(id);
+  }
+
+  public async Task<EntityEntry<Employee>> CreateAsync(Employee employee)
+  {
+    return await employeeRepository.CreateAsync(employee);
+  }
+
+  public async Task<int> UpdateAsync(Guid id, UpdateEmployeeDto updateEmployeeDto)
+  {
+    return await employeeRepository.UpdateAsync(id, updateEmployeeDto);
+  }
+
+  public async Task DeleteAsync(Guid id)
+  {
+    await employeeRepository.DeleteAsync(id);
+  }
+}
